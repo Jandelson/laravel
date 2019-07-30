5fc8bc79621b6c77d7f4f7d00261772e99ac69a0
Jandelson Oliveira <jandelson_oliveira@yahoo.com.br>
2019-07-30 14:13:24 -0300

    Rotas com novo name Space

---
+++ app/Http/Controllers/Test/UserController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Test;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\User;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+    	$users = \App\User::all();
+    	return $users;
+    }
+
+    public function show($id) 
+    {
+    	$users = User::findOrFail($id);
+    	return $users;
+    }
+}
